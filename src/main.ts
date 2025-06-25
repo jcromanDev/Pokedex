b38d00cad2ee1fb04b5f5a5ee1cb2bf773e198be
jcromanDev <jcroman.dev@outlook.com>
2025-06-25 12:24:20 -0600

    footer completed fix bugs

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import "./style/modern-normalize.css";
 import "./style/style.css";
 import "./style/components/header.css";
 import "./style/components/pokemon.css";
+import "./style/components/footer.css";
 
 import "./style/utils.css";
